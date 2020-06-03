c54e59ad220903f1d8aa96dd3263f3b12013a50b
Donatien Criaud <30463908+DonaCrio@users.noreply.github.com>
2020-06-03 20:30:55 +0200

    Fix CI (#1)

--- ci/scripts/build-musl-debug.sh
+++ ci/scripts/build-musl-debug.sh
@@ -1,7 +1,7 @@
  
 #!/bin/bash
 
-# Usage: ./musl-build-debug.sh <MODULE_NAME>
+# Usage: ./build-musl-debug.sh <MODULE_NAME>
 #
 # Most parts of this script are comming from:
 # https://github.com/emk/rust-musl-builder/blob/master/examples/build-release
@@ -18,4 +18,3 @@ docker run -it --name "$1" "$1"-debug
 docker cp "$1":/home/rust/src/target/x86_64-unknown-linux-musl/debug/"$1" ./target/x86_64-unknown-linux-musl/debug/"$1"
 docker rm "$1"
 docker rmi "$1"-debug
-       
